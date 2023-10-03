e42f797d24626db4f0c489240595af1d066462f1
Mkheir13 <kheireddine.mederreg@ynov.com>
2023-10-03 22:33:36 +0200

    refractor the menu, the medias, and ending the new collection

---
+++ frontend/frontpage.js
@@ -0,0 +1,21 @@
+let header = document.querySelector("header");
+let menu = document.querySelectorAll("a");
+
+document.addEventListener("scroll", () => {
+    let scrollPosition = window.scrollY;
+    if (scrollPosition > 10) {
+        header.style.backgroundColor = "#F2F2F4";
+        header.style.color = "#000000";
+        menu.forEach((item) => {
+            item.style.color = "#000000";
+        });
+        header.style.transition = "background-color 1s ease-in-out, color 1s ease-in-out";
+    } else {
+        header.style.backgroundColor = "transparent";
+        header.style.color = "#FFFFFF";
+        menu.forEach((item) => {
+            item.style.color = "#FFFFFF";
+        });
+        header.style.transition = "background-color 1s ease-in-out, color 1s ease-in-out";
+    }
+});
