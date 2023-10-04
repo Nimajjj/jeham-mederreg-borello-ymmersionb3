a4abffd49eb61a96de3ab5b4017d374fc626eff9
Mkheir13 <kheireddine.mederreg@ynov.com>
2023-10-04 10:06:15 +0200

    push a la une

--- frontend/frontpage.js
+++ frontend/frontpage.js
@@ -3,8 +3,9 @@ let menu = document.querySelectorAll("a");
 
 document.addEventListener("scroll", () => {
     let scrollPosition = window.scrollY;
-    if (scrollPosition > 10) {
+    if (scrollPosition > 0) {
         header.style.backgroundColor = "#F2F2F4";
+        header.style.borderBottom = "1px solid #000000";
         header.style.color = "#000000";
         menu.forEach((item) => {
             item.style.color = "#000000";
@@ -13,6 +14,7 @@ document.addEventListener("scroll", () => {
     } else {
         header.style.backgroundColor = "transparent";
         header.style.color = "#FFFFFF";
+        header.style.borderBottom = "none";
         menu.forEach((item) => {
             item.style.color = "#FFFFFF";
         });
