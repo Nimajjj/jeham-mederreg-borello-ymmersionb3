519a13cf1276fbd70df56ca819c5178a0dabccc0
Nimajjj <benjamin.borello@aol.com>
2023-10-04 09:33:59 +0200

    chore(json): finish import json to db

--- backend/repo/pebbles_repo.go
+++ backend/repo/pebbles_repo.go
@@ -30,22 +30,32 @@ func (pr *PebbleRepo) InsertNewPebble(pebble *model.Pebble) {
         return 
     }
 
-
-    // insert categories
-    categories_repo := NewCategorieRepo()
-    for _, cat := range pebble.Categories {
-        categories_repo.InsertNewCategorie(cat)
-    }
-
-    // insert photos
-
-
-    // finally insert pebble
+    // insert pebble
     query = "INSERT INTO pebbles (Title, Description, Price, Breed, Weight, Quantity, Creation) VALUES (?, ?, ?, ?, ?, ?, ?);"
 
     _, err = pr.db.Exec(query, pebble.Title, pebble.Description, pebble.Price, pebble.Breed, pebble.Weight, pebble.Quantity, time.Now().Format("01-02-2006"))
     if err != nil {
       panic(err) 
+    }
+
+    // get pebble id
+    query = "SELECT ID FROM pebbles WHERE title = ?"
+    var id int
+    err = pr.db.QueryRow(query, pebble.Title).Scan(&id)
+    if err != nil {
+      panic(err)
+    }
+
+    // insert categories
+    categories_repo := NewCategorieRepo()
+    for _, cat := range pebble.Categories {
+        categories_repo.InsertNewCategorie(cat, id)
+    }
+    
+    // insert photos
+    photo_repo := NewPhotoRepo()
+    for _, photo := range pebble.Photos {
+        photo_repo.InsertNewPhoto(photo, id)
     }
 
     fmt.Print("try InsertNewPebble -> ", pebble.Title, " -> SUCCESS\n")
